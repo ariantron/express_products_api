28ea6c5de5c679cbb93c55430e00b40c86959efe
Arian Tron <ariantron@yahoo.com>
2023-03-31 02:09:02 +0430

    connect to db at app.ts & edit error message at db.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import config from "config"
+import connect from "./utills/db";
 
 const app = express()
 
 const port = config.get<number>("port")
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log('App is running !')
+    await connect()
 })
--- src/utills/db.ts
+++ src/utills/db.ts
@@ -7,7 +7,7 @@ async function connect() {
         await mongoose.connect(dbUri)
         console.log('Connected to DB')
     } catch (error) {
-        console.error('Could not connected to DB')
+        console.error(`Could not connected to DB because:\nError: ${error}`)
         process.exit(1)
     }
 }
