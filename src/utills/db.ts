6087fbbae40db3f34a73606509e986c217837679
Arian Tron <ariantron@yahoo.com>
2023-03-31 01:31:21 +0430

    create db connection function

---
+++ src/utills/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+import config from "config"
+
+async function connect() {
+    const dbUri = config.get<string>('dbUri')
+    try {
+        await mongoose.connect(dbUri)
+        console.log('Connected to DB')
+    } catch (error) {
+        console.error('Could not connected to DB')
+        process.exit(1)
+    }
+}
+
+export default connect
