73bce1b341c67366273c2651b870c6fb1f656c8a
Arian Tron <ariantron@yahoo.com>
2023-03-31 12:34:11 +0430

    write first api (test)

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
-import {Express} from "express"
+import {Express, Request, Response} from "express"
 
 function routes(app: Express) {
-
+    app.get('/test', (request: Request, response: Response) => {
+        response.sendStatus(200)
+    })
 }
 
 export default routes
