1fc17de2c20c84f9c19ef77b4075c56893507c52
Arian Tron <ariantron@yahoo.com>
2023-04-01 12:41:23 +0430

    create session service create function

---
+++ src/services/session.service.ts
@@ -0,0 +1,14 @@
+import SessionModel from "../models/session.model";
+
+class SessionService {
+    static async create(userId: string, userAgent: string) {
+        try {
+            const session = await SessionModel.create({userId, userAgent})
+            return session.toJSON()
+        } catch (error: any) {
+            throw new Error(error)
+        }
+    }
+}
+
+export default SessionService
