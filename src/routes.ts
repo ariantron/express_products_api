83d9a02f6f789968d59752600a369dac24f1d28e
Arian Tron <ariantron@yahoo.com>
2023-04-01 11:02:25 +0430

    add create user api

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,7 @@
 import {Express, Request, Response} from "express"
+import validateResource from "./middlewares/validateResource";
+import {createUserSchema} from "./schemas/user.schema";
+import UserController from "./controllers/user.controller";
 
 function routes(app: Express) {
 
@@ -6,6 +9,11 @@ function routes(app: Express) {
     app.get('/test', (request: Request, response: Response) => {
         response.sendStatus(200)
     })
+
+    //users
+    //--create user
+    app.post('/users',validateResource(createUserSchema),UserController.create)
+
 }
 
 export default routes
