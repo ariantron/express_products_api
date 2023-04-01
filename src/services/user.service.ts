737e4b0b7ca21b7d76f882b9f3fcf0b268d51587
Arian Tron <ariantron@yahoo.com>
2023-04-01 12:22:21 +0430

    fix bug

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,7 +5,7 @@ class UserService {
     static async create(input: UserInput) {
         try {
             const user = await UserModel.create(input)
-            return omit(user.toJSON, "password")
+            return omit(user.toJSON(), "password")
         } catch (error: any) {
             throw new Error(error)
         }
