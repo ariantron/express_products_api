5f7a8dad40e831e723e42d49ea9e2b0aea5309f1
Arian Tron <ariantron@yahoo.com>
2023-04-01 10:52:59 +0430

    create userCreateSchema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import {object, string} from "zod";
+
+export const createUserSchema = object({
+    body: object({
+        name: string({
+            required_error: "Name is required"
+        }),
+        email: string({
+            required_error: "Email is required"
+        }).email("Email is not valid"),
+        password: string({
+            required_error: "Password is required"
+        }).min(6, "Password is too short, should be 6 chars minimum"),
+        passwordConfirmation: string({
+            required_error: "Password Confirmation is required"
+        }),
+    }).refine((data)=>data.password===data.passwordConfirmation,{
+        message:"Password do not match",
+        path:["passwordConfirmation"]
+    })
+})
