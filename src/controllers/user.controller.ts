9976a4cbb2eea964041420b82a2bcc11748df048
Arian Tron <ariantron@yahoo.com>
2023-04-01 11:10:09 +0430

    update create action

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,11 +4,11 @@ import logger from "../utills/logger";
 import {CreateUserInput} from "../schemas/user.schema";
 
 class UserController {
-    static async create(request: Request<{}, {}, CreateUserInput>, response: Response) {
+    static async create(request: Request<{}, {}, CreateUserInput["body"]>, response: Response) {
         try {
             const user = await UserService.create(request.body)
             return response.send(user)
-        } catch (error) {
+        } catch (error: any) {
             logger.error(error)
             return response.status(409).send(error.message)
         }
