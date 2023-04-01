6c6bac40563f068cdd6ed4aef38dcebb3261e297
Arian Tron <ariantron@yahoo.com>
2023-04-01 12:21:57 +0430

    exclude password confirmation for insert to input

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -20,4 +20,4 @@ export const createUserSchema = object({
     })
 })
 
-export type CreateUserInput = TypeOf<typeof createUserSchema>
+export type CreateUserInput = Omit<TypeOf<typeof createUserSchema>, "body.passwordConfirmation">
