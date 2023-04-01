e22970d3de78a3ca5b2efcf93903cb39a155ccb1
Arian Tron <ariantron@yahoo.com>
2023-04-01 10:06:26 +0430

    update code with creation of UserService class

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,15 @@
 import UserModel, {UserInput} from "../models/user.model"
 import {omit} from "lodash"
 
-export async function createUser(input: UserInput) {
-    try {
-        const user = await UserModel.create(input)
-        return omit(user.toJSON, "password")
-    } catch (error: any) {
-        throw new Error(error)
+class UserService {
+    static async create(input: UserInput) {
+        try {
+            const user = await UserModel.create(input)
+            return omit(user.toJSON, "password")
+        } catch (error: any) {
+            throw new Error(error)
+        }
     }
 }
+
+export default UserService
