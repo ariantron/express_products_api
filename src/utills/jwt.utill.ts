d1dc048959a9757033a3d9c73f0332d77ddc3f74
Arian Tron <ariantron@yahoo.com>
2023-04-02 23:33:55 +0430

    add signJwt, verifyJwt functions

--- src/utills/jwt.utill.ts
+++ src/utills/jwt.utill.ts
@@ -1,12 +1,38 @@
+import jwt from "jsonwebtoken"
+import TokenType from "../enums/tokenType.enum"
 import config from "config"
+import logger from "./logger.utill";
 
-const privateKey = config.get<string>("privateKey")
-const publicKey = config.get<string>("publicKey")
-
-function signJwt() {
-
+export function signJwt(
+    object: Object,
+    keyName: TokenType.AccessTokenPrivateKey | TokenType.RefreshTokenPrivateKey,
+    options?: jwt.SignOptions | undefined
+) {
+    const signingKey = Buffer.from(config.get<string>(keyName), "base64").toString("ascii")
+    logger.info(signingKey)
+    return jwt.sign(object, signingKey,
+        {
+            ...(options && options),
+            algorithm: "RS256"
+        }
+    )
 }
 
-function verifyJwt() {
-
+export function verifyJwt(token: string,
+                          keyName: TokenType.AccessTokenPublicKey | TokenType.RefreshTokenPublicKey) {
+    const publicKey = Buffer.from(config.get<string>(keyName), "base64").toString("ascii")
+    try {
+        const decoded = jwt.verify(token, publicKey)
+        return {
+            valid: true,
+            expired: false,
+            decoded
+        }
+    } catch (error: any) {
+        return {
+            valid: true,
+            expired: error.message === 'jwt expired',
+            decoded: null
+        }
+    }
 }
