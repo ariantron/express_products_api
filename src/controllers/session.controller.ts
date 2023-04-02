b9a91f8049dcc528dcc99b555fd4e34f014519cb
Arian Tron <ariantron@yahoo.com>
2023-04-02 23:32:29 +0430

    complete create user session function

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,20 +1,38 @@
-import {Request, Response} from "express";
+import {Request, Response} from "express"
 import UserService from "../services/user.service"
-import {HttpStatusCode} from "../enums/httpStatusCode.enum";
+import {HttpStatusCode} from "../enums/httpStatusCode.enum"
+import SessionService from "../services/session.service"
+import {signJwt} from "../utills/jwt.utill"
+import config from "config"
+import TokenType from "../enums/tokenType.enum"
 
 class SessionController {
-    static async create(request:Request,response:Response) {
+    static async create(request: Request, response: Response) {
         //Validate the user's password
-        const user = UserService.validatePassword(request.body)
-        if(!user)
+        const user = await UserService.validatePassword(request.body)
+        if (!user)
             return response.status(HttpStatusCode.Unauthorized).send("Invalid email or password")
 
         //Create a session
+        const session = await SessionService.create(user._id, request.get("user-agent") || "")
 
         //Create an access token
+        const accessToken = signJwt(
+            {...user, session: session._id},
+            TokenType.AccessTokenPrivateKey,
+            {expiresIn: config.get('accessTokenTtl')}
+        )
 
         //Create a refresh token
+        const refreshToken = signJwt(
+            {...user, session: session._id},
+            TokenType.RefreshTokenPrivateKey,
+            {expiresIn: config.get('refreshTokenTtl')}
+        )
 
         //Access to access and refresh token
+        return response.send({accessToken, refreshToken})
     }
 }
+
+export default SessionController
