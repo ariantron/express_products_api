9701a81decd0487ed129eef65735b241b4cb6c8b
Arian Tron <ariantron@yahoo.com>
2023-04-02 23:31:19 +0430

    Add TokenType Enum

---
+++ src/enums/tokenType.enum.ts
@@ -0,0 +1,8 @@
+enum TokenType {
+    AccessTokenPublicKey = "accessTokenPublicKey",
+    AccessTokenPrivateKey = "accessTokenPrivateKey",
+    RefreshTokenPublicKey = "refreshTokenPublicKey",
+    RefreshTokenPrivateKey = "refreshTokenPrivateKey"
+}
+
+export default TokenType
