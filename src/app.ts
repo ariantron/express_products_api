7b8950d6ba8892c14ec842a2db3503a48b12221c
Arian Tron <ariantron@yahoo.com>
2023-04-03 01:52:27 +0430

    set user session after login

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import config from "config"
 import connect from "./utills/db.utill"
 import logger from "./utills/logger.utill"
 import routes from "./routes"
+import deserializeUser from "./middlewares/deserializeUser.middleware"
 
 const app = express()
 
 app.use(express.json())
+
+app.use(deserializeUser)
 
 const port = config.get<number>("port")
 
